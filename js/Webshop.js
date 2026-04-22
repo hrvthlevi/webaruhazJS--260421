72b6e4a7c35eea8ac939a4549a1d78bf7820f2f7
hrvthlevi <horvathleventeroland@gmail.com>
2026-04-22 19:30:27 +0200

    feat: webshop osztaly megvalositasa privat mezokkel es nezetvaltas logikaval

---
+++ js/Webshop.js
@@ -0,0 +1,39 @@
+// Webshop.js
+import { SAKKOZOK } from "./adat.js";
+
+export class Webshop {
+  #termekek;
+  #kosar;
+
+  constructor() {
+    this.szuloElem = document.querySelector("#main-content");
+    this.init();
+  }
+
+  init() {
+    this.#termekek = new Termekek(SAKKOZOK, this.szuloElem);
+    this.esemenyFigyeles();
+  }
+
+  esemenyFigyeles() {
+    const gombok = document.querySelectorAll("nav button");
+    gombok.forEach((gomb) => {
+      gomb.addEventListener("click", () => {
+        const tipus = gomb.getAttribute("data-view");
+        this.nezetValtas(tipus);
+      });
+    });
+  }
+
+  nezetValtas(tipus) {
+    this.szuloElem.innerHTML = "";
+
+    if (tipus === "shop") {
+      this.#termekek = new Termekek(SAKKOZOK, this.szuloElem);
+    } else if (tipus === "kosar") {
+      this.szuloElem.innerHTML = "<h2>A kosár tartalma hamarosan...</h2>";
+    } else if (tipus === "admin") {
+      this.szuloElem.innerHTML = "<h2>Admin felület hamarosan...</h2>";
+    }
+  }
+}
